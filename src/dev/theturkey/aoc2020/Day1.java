b15dc4f66ba395682b454d02218274162e40e4a4
TheTurkeyDev <turkey@theturkey.dev>
2020-12-01 00:13:31 -0500

    Day 1 Code

--- src/dev/theturkey/aoc2020/Day1.java
+++ src/dev/theturkey/aoc2020/Day1.java
@@ -12,6 +12,22 @@ public class Day1 extends AOCPuzzle
 	@Override
 	void solve(List<String> input)
 	{
-
+		for(int i = 0; i < input.size(); i++)
+		{
+			for(int j = i + 1; j < input.size(); j++)
+			{
+				for(int k = j + 1; k < input.size(); k++)
+				{
+					Integer int1 = Integer.parseInt(input.get(i));
+					Integer int2 = Integer.parseInt(input.get(j));
+					Integer int3 = Integer.parseInt(input.get(k));
+					if(int1 + int2 + int3 == 2020)
+					{
+						System.out.println("Answer: " + int1 * int2 * int3);
+						return;
+					}
+				}
+			}
+		}
 	}
 }
