b1cbca0b1214717222915f8fa6e301459dd9dc53
TheTurkeyDev <turkey@theturkey.dev>
2020-12-02 00:33:19 -0500

    Day 2 Code

--- src/dev/theturkey/aoc2020/AoC2020Core.java
+++ src/dev/theturkey/aoc2020/AoC2020Core.java
@@ -4,6 +4,7 @@ public class AoC2020Core
 {
 	public static void main(String[] args)
 	{
-		new Day1();
+		//new Day1();
+		new Day2();
 	}
 }
---
+++ src/dev/theturkey/aoc2020/Day2.java
@@ -0,0 +1,60 @@
+package dev.theturkey.aoc2020;
+
+import java.util.List;
+
+public class Day2 extends AOCPuzzle
+{
+	public Day2()
+	{
+		super("2");
+	}
+
+	@Override
+	void solve(List<String> input)
+	{
+		/* PART 1*/
+		int correct = 0;
+		for(String s : input)
+		{
+			String[] parts = s.split(" ");
+			String[] minMax = parts[0].split("-");
+			int min = Integer.parseInt(minMax[0]);
+			int max = Integer.parseInt(minMax[1]);
+			char letter = parts[1].charAt(0);
+
+			int occurrences = 0;
+			for(char c : parts[2].toCharArray())
+			{
+				if(c == letter)
+				{
+					occurrences++;
+				}
+			}
+
+			if(occurrences >= min && occurrences <= max)
+			{
+				correct++;
+			}
+		}
+
+		System.out.println("Part 1 Correct: " + correct);
+
+		/* PART 2*/
+		correct = 0;
+		for(String s : input)
+		{
+			String[] parts = s.split(" ");
+			String[] minMax = parts[0].split("-");
+			int pos1 = Integer.parseInt(minMax[0]) - 1;
+			int pos2 = Integer.parseInt(minMax[1]) - 1;
+			char letter = parts[1].charAt(0);
+
+			if(parts[2].charAt(pos1) == letter ^ parts[2].charAt(pos2) == letter)
+			{
+				correct++;
+			}
+		}
+
+		System.out.println("Part 2 Correct: " + correct);
+	}
+}
