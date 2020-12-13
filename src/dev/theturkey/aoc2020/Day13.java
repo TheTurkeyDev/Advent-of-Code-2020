605733103ebece4db459cdcd3c09796f7ea3ef1c
TheTurkeyDev <turkey@theturkey.dev>
2020-12-13 15:54:03 -0500

    Day 13 Part 2 improved solution

--- src/dev/theturkey/aoc2020/Day13.java
+++ src/dev/theturkey/aoc2020/Day13.java
@@ -46,42 +46,30 @@ public class Day13 extends AOCPuzzle
 
 
 		// PART 2 uses the Chinese Remainder Theorem... I don't know it though...
-		// The below was what I got and it works, but is not based off the Chinese Remainder Theorem
-		// and is therefore not efficient enough to solve the problem quick enough
+		// The below doesn't use CRT as isn't my own work. It's a solution that was hashed out with the help of others
 		busIDs = new ArrayList<>();
-		boolean complete = false;
-		int t = 0;
-		int largestIndex = 0;
+
 		for(String id : inputStrs.get(1).split(","))
 		{
 			if(id.equals("x"))
 				busIDs.add(0);
 			else
 				busIDs.add(Integer.parseInt(id));
-
-			if(busIDs.get(largestIndex) < busIDs.get(busIDs.size() - 1))
-			{
-				largestIndex = busIDs.size() - 1;
-			}
 		}
 
-		while(!complete)
+		long step = 1;
+		long out = 0;
+		for(int i = 0; i < busIDs.size(); i++)
 		{
-			t += busIDs.get(largestIndex);
-			complete = true;
-			for(int i = 0; i < busIDs.size(); i++)
-			{
-				if(busIDs.get(i) == 0)
-					continue;
+			int busID = busIDs.get(i);
+			if(busID == 0)
+				continue;
 
-				if((t - largestIndex + i) % busIDs.get(i) != 0)
-				{
-					complete = false;
-					break;
-				}
-			}
+			while((out + i) % busID != 0)
+				out += step;
+
+			step *= busID;
 		}
-
-		lap(t - largestIndex);
+		lap(out);
 	}
 }
